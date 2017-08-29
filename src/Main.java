45b0091ea63705e0bde00bc6944d922c84417bc6
shinabarger <davidshinabarger@gmail.com>
2017-08-29 19:30:00 -0400

    add bufferedReader and fileReader so that it reads the file and prints it out line by line. solid start.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import com.test.Reading_With_Exceptions;
 
 public class Main {
 
+    private static final String fileName = "/Users/davidshinabarger/Desktop/CPS261/Week1/JH0/testFiles/firstTest.txt";
+
     public static void main(String[] args) {
 
-        Reading_With_Exceptions.process("Test");
+        Reading_With_Exceptions.process(fileName);
 
     }
 }
--- src/com/test/Reading_With_Exceptions.java
+++ src/com/test/Reading_With_Exceptions.java
@@ -1,6 +1,8 @@
 package com.test;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by davidshinabarger on 8/29/17.
@@ -10,9 +12,28 @@ public class Reading_With_Exceptions {
 
     public static void process(String inputFileName) {
 
-        File inputFile = new File(inputFileName);
-        System.out.println(inputFileName);
+        BufferedReader bufferedReader = null;
+        FileReader fileReader = null;
 
+        try {
+            fileReader = new FileReader(inputFileName);
+            bufferedReader = new BufferedReader(fileReader);
+
+            String currentLine;
+
+            while((currentLine = bufferedReader.readLine()) != null) {
+                System.out.println(currentLine);
+            }
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+                fileReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
